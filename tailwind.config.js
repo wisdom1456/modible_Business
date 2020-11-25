099179fc03a0a58daef2d5cba0a67d21aa53caf0
wisdom1456 <Modible@gmail.com>
2020-11-25 01:48:05 -0600

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,44 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        'ws-blue': {
-          100: '#E6F0FF',
-          200: '#BFDAFF',
-          300: '#99C3FF',
-          400: '#4D97FE',
-          500: '#006AFE',
-          600: '#005FE5',
-          700: '#004098',
-          800: '#003072',
-          900: '#00204C',
-        },
-      }
+    fontFamily: {
+      title: ['RobotoSlab'],
+      body: ['Neutron'],
     },
-  },
-  variants: {},
-  plugins: [],
-  purge: process.env.NODE_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
-}
+		extend: {
+      colors: {
+        "ws-blue": {
+          100: "#E6F0FF",
+          200: "#BFDAFF",
+          300: "#99C3FF",
+          400: "#4D97FE",
+          500: "#006AFE",
+          600: "#005FE5",
+          700: "#004098",
+          800: "#003072",
+          900: "#00204C",
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+        },
+        blue: {
+          DEFAULT: '8EF2FF',
+        },
+        orange: {
+          DEFAULT: '#E36E40',
+        },
+        brown: {
+          DEFAULT: '#161819',
+        }
+      },
+		},
+	},
+	variants: {},
+	plugins: [],
+	purge:
+		process.env.NODE_ENV === "production"
+			? {
+					enabled: true,
+					content: ["src/**/*.njk", "src/**/*.js"],
+			  }
+			: {},
+};
