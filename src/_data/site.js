9ae292efd9fff0c871eae4fa0a5504ed0424751a
wisdom1456 <Modible@gmail.com>
2020-11-25 21:19:48 -0600

    Updated for Light SEO

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "title": "Web Starter",
-  "description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS",
-  "url": "https://webstarter.chriscollins.me",
-  "author": "Chris Collins",
+  "title": "Modible",
+  "description": "Franklin Riley | Web Developer | Niceville, Florida",
+  "url": "https://modnow.netlify.app",
+  "author": "Franklin Riley",
   "meta_data": {
     "twitter": "@scottishstoater",
     "default_social_image": "/static/default_social_image.jpg"
