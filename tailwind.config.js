21c587e279e26148a735a923ec04b67a0f9977bd
wisdom1456 <Modible@gmail.com>
2020-11-27 18:57:31 -0600

    Fixed font issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,49 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      title: ['RobotoSlab'],
-      body: ['Neutron'],
+    extend: {
+      fontFamily: {
+        roboto: ["RobotoSlab", "serif"],
+        neuton: ["Neuton", "serif"]
+      }
     },
 		extend: {
-      colors: {
-        "ws-blue": {
-          100: "#E6F0FF",
-          200: "#BFDAFF",
-          300: "#99C3FF",
-          400: "#4D97FE",
-          500: "#006AFE",
-          600: "#005FE5",
-          700: "#004098",
-          800: "#003072",
-          900: "#00204C",
-        },
-        green: {
-          500: '#8CA653',
-        },
-        ltblue: {
-          500: '#A9C6D9',
-        },
-        blue: {
-          500: '#049DBF',
-        },
-        orange: {
-          500: '#BF491F',
-        },
-        yellow: {
-          500: '#D9BD32',
-        }
-      },
+			colors: {
+				"ws-blue": {
+					100: "#E6F0FF",
+					200: "#BFDAFF",
+					300: "#99C3FF",
+					400: "#4D97FE",
+					500: "#006AFE",
+					600: "#005FE5",
+					700: "#004098",
+					800: "#003072",
+					900: "#00204C",
+				},
+				green: {
+					500: "#8CA653",
+					600: "#8CA653",
+				},
+				ltblue: {
+					500: "#A9C6D9",
+					600: "#A9C6D9",
+				},
+				blue: {
+					500: "#049DBF",
+					600: "#049DBF",
+				},
+				orange: {
+					500: "#BF491F",
+					600: "#BF491F",
+				},
+				yellow: {
+					500: "#D9BD32",
+					600: "#D9BD32",
+				},
+			},
 		},
 	},
 	variants: {},
-	plugins: [],
+	plugins: [require("@tailwindcss/typography")],
 	purge:
 		process.env.NODE_ENV === "production"
 			? {
