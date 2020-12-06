bc662d88762d24667817758a4354894f2ec7ac59
wisdom1456 <Modible@gmail.com>
2020-12-06 03:57:05 -0600

    Updated layouts on contact page

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -12,7 +12,7 @@ for (let i = 0; i < numBalls; i++) {
     let ball = document.createElement("div");
     ball.classList.add("ball");
     ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.left = `${Math.floor(Math.random() * 70)}vw`;
     ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
     ball.style.transform = `scale(${Math.random()})`;
     ball.style.width = `${Math.random()}em`;
